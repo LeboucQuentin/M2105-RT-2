a499d8041789f3e354fcc12960262a6f47e659d2
QuentinLebouc <21507822@etu.unicaen.fr>
2016-03-17 17:39:44 +0100

    fonction

---
+++ tp11/tp2/ex2.php
@@ -0,0 +1,26 @@
+<?php
+
+include './functions.php';
+if(array_key_exists("nom",$_COOKIE))
+        header("location:recoAuto.php");
+getHeader(true,"Exercice 2");
+?>
+    <div class="container">
+        <form class="form-inline" method="post" action="exo2validation.php">
+                <fieldset>
+                        <legend>Connexion</legend>
+                        <div class="alert alert-info">Saisir votre nom et valider.</div>
+                        <div class="form-group">
+                                <input placeholder="Nom..." class="form-control" type="text" name="nom">
+                                <input class="btn btn-primary" type="submit" value="Valider">
+                            </div>
+                        <div class="form-group">
+                                <label class="control-label"><input type="checkbox" name="ckRemember"> Se souvenir de moi</label>
+                            </div>
+                    </fieldset>
+
+            </form>
+    </div>
+
+    <?php
+getFooter();
