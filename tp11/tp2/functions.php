9601d63808952fd27b0460dbd9f8dfa61217dc46
QuentinLebouc <21507822@etu.unicaen.fr>
2016-03-17 17:27:22 +0100

    fonction

--- tp11/tp2/functions.php
+++ tp11/tp2/functions.php
@@ -1,22 +1,18 @@
 <?php
 
-function getHeader($startSession,$title)
-{
-
-
-    if ($startSession)
-        session_start();
-
-    echo "
-        <!doctype html>
-        <html>
-        <head>
-            <meta charset='utf-8'>
-            <title>{$title}</title>
-            
-</head>
-</html>
-<header>
-    <h1>$title</h1>
-    </header>";
+function getHeader($startSession, $title){
+        if ($startSession)
+            session_start();
+     echo "
+	 <!doctype html>
+	 <html>
+	 <head>
+	    <meta charset='utf-8'>
+	    <title>{$title}</title>
+	        <link rel='stylesheet' href='./bootstrap.min.css'>
+	</head>
+	<body>";
+}
+function getFooter(){
+    echo "</body></html>";
 }
